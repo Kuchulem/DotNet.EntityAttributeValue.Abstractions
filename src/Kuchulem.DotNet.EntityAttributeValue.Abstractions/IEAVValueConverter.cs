a3d84e7aede4ae5ee421258c9712765bf2d9060b
Anthony Maudry <amaudry@gmail.com>
2023-10-01 20:53:39 +0200

    Added missing documentation

--- src/Kuchulem.DotNet.EntityAttributeValue.Abstractions/IEAVValueConverter.cs
+++ src/Kuchulem.DotNet.EntityAttributeValue.Abstractions/IEAVValueConverter.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace Kuchulem.DotNet.EntityAttributeValue.Abstractions
 {
+    /// <summary>
+    /// Interface for classes that converts a string value (as stored in DB) to
+    /// a usable value and back
+    /// </summary>
+    /// <typeparam name="T">The expected type for this value</typeparam>
     public interface IEAVValueConverter<T>
     {
+        /// <summary>
+        /// Converts a string value to a usable value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         T Convert(string value);
 
+        /// <summary>
+        /// Converts a usable value to a string storable in DB
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         string ConvertBack(T value);
     }
 }
